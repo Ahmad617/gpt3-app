0e86fb411abd5ed9d1b81d4222973eafe70f791a
ahmad617 <ahmadrafaqat.617@gmail.com>
2022-02-13 10:02:00 +0500

    CTA section completed.!

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './whatgpt3.css'
+import React from 'react';
+import { Feature } from '../../components';
+import './whatgpt3.css';
 
 function WhatGPT3() {
   return (
-    <div>WhatGPT3</div>
+    <div className="whatgpt3 section_margin" id='wgpt3'>
+      <div className="wgpt3_feature">
+        <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard of ought.His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+      </div>
+      <div className="wgpt3_heading">
+        <h1 className="gradient_text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="wgpt3_container">
+        <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
+        <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+        <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+
+      </div>
+    </div>  
   )
 }
 
-export default WhatGPT3
+export default WhatGPT3;

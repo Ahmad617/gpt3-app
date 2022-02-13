5fd83a337700f049191d1df2c2962defe89a577d
ahmad617 <ahmadrafaqat.617@gmail.com>
2022-02-13 07:14:37 +0500

    First commit, basic folder structure created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Blog, Footer, Features, Header, Possibility, WhatGPT3 } from './containers';
+import { Article, Brand, CTA, Feature, Navbar } from './components';
+
+function App() {
+
+  return (
+    <div className="App">
+    </div>
+  )
+}
+
+export default App

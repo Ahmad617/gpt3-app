2c37e305fb571de65945ad6d8108b8aa8264f8b1
ahmad617 <ahmadrafaqat.617@gmail.com>
2022-02-13 10:03:21 +0500

    CTA section completed.!

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -4,7 +4,15 @@ import './cta.css'
 
 function CTA() {
   return (
-    <div>CTA</div>
+    <div className="cta section_padding">
+      <div className="cta_content">
+        <p>Request Early Access to Get Stared</p>
+        <h3>Register below & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="cta_button">
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 

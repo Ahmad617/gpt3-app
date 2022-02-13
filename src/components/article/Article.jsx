fb83fa43f47b23e9b844bbb1f3045cbb73e703d7
ahmad617 <ahmadrafaqat.617@gmail.com>
2022-02-13 15:32:20 +0500

    Blog section completed.!

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import './article.css'
+import React from 'react';
+import './article.css';
 
 
-function Article() {
+function Article({ imgUrl, date, title }) {
   return (
-    <div>Article</div>
+    <div className="article">
+      <div className="article_image">
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="article_content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-import './blog.css'
-
+import React from "react";
+import Article from "../../components/article/Article";
+import "./blog.css";
+import {getCurrentDate} from './Date'
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
 
 function Blog() {
+  const Date= getCurrentDate();
+
   return (
-    <div>Blog</div>
-  )
+    <div className="blog section_padding" id="blog">
+      <div className="blog_heading">
+        <h1 className="gradient_text">
+          A lot is happining, We are blogging about it.
+        </h1>
+      </div>
+      <div className="blog_container">
+        <div className="blog-container_groupA">
+          <Article imgUrl={blog01} date={Date} title="GPT-3 and Open AI is the future. Let us explore how it is?" />
+        </div>
+        <div className="blog-container_groupB">
+          <Article imgUrl={blog02} date={Date} title="GPT-3 and Open AI is the future. Let us explore how it is?" />
+          <Article imgUrl={blog03} date={Date} title="GPT-3 and Open AI is the future. Let us explore how it is?" />
+          <Article imgUrl={blog04} date={Date} title="GPT-3 and Open AI is the future. Let us explore how it is?" />
+          <Article imgUrl={blog05} date={Date} title="GPT-3 and Open AI is the future. Let us explore how it is?" />
+        </div>
+        
+      </div>
+    </div>
+  );
 }
 
-export default Blog
+export default Blog;

8a25c1a119dd0f9f1534b130a1181c2b51c89458
ahmad617 <ahmadrafaqat.617@gmail.com>
2022-02-13 07:41:27 +0500

    Brand section completed.!

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './brand.css';
-
+import { google, slack, atlassian, dropbox, shopify } from './imports';
 
 function Brand() {
   return (
-    <div>Brand</div>
+    <div className="Brand section_padding" >
+      <div className="">
+        <img src={google} alt="google" />
+      </div>
+      <div className="">
+        <img src={slack} alt="slack" />
+      </div>
+      <div className="">
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div className="">
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div className="">
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
   )
 }
 
